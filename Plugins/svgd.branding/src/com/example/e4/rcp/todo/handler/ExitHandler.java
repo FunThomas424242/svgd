fc33b5d281a05744ff3314a980807298d72c594e
Thomas Schubert <funthomas424242@googlemail.com>
2013-03-02 19:14:58 +0100

    key bindings added

--- Plugins/svgd.branding/src/com/example/e4/rcp/todo/handler/ExitHandler.java
+++ Plugins/svgd.branding/src/com/example/e4/rcp/todo/handler/ExitHandler.java
@@ -2,10 +2,13 @@ package com.example.e4.rcp.todo.handler;
 
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.IWorkbench;
 
 public class ExitHandler {
-  @Execute
-  public void execute() {
-    System.out.println("ExitHandler Called");
-  }
-} 
+
+	@Execute
+	public void execute(IWorkbench workbench) {
+		workbench.close();
+	}
+
+}
--- Plugins/svgd.branding/src/com/example/e4/rcp/todo/handler/SaveAllHandler.java
+++ Plugins/svgd.branding/src/com/example/e4/rcp/todo/handler/SaveAllHandler.java
@@ -4,8 +4,8 @@ import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 
 public class SaveAllHandler {
-  @Execute
-  public void execute() {
-    System.out.println("SaveAllHandler Called");
-  }
+	@Execute
+	public void execute() {
+		System.out.println("SaveAllHandler Called");
+	}
 } 
